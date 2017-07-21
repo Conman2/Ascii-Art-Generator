3c7e86797026ef1251895439799fa4a00effc11f
Conman2 <richardharris443@gmail.com>
2017-07-21 17:29:10 +1000

    Quick update

--- Main.py
+++ Main.py
@@ -13,14 +13,14 @@ cell_size = 1
 img_xrange = range(0, img_width, cell_size)
 img_yrange = range(0, img_height, cell_size)
 
-The_Matrix = numpy.zeros((len(img_xrange), len(img_yrange), 3))
+The_Matrix = numpy.zeros((len(img_xrange), len(img_yrange)))
 
 def Average_Colour(pixel_x, pixel_y):
     colour_array_red = []
     colour_array_green = []
     colour_array_blue = []
 
-    #Getting the Average Values for each Section
+    #Getting the Average RGB Values for each Section
     for thing3 in range(pixel_x*cell_size, pixel_x*cell_size + cell_size):
         for thing4 in range(pixel_y*cell_size, pixel_y*cell_size + cell_size):
             colour = pixel[thing3, thing4]
@@ -28,23 +28,35 @@ def Average_Colour(pixel_x, pixel_y):
             colour_array_green.append(colour[1])
             colour_array_blue.append(colour[2])
 
-    avg_colour = ([sum(colour_array_red)/len(colour_array_red), sum(colour_array_green)/len(colour_array_green), sum(colour_array_blue)/len(colour_array_blue)]*[0.299, 0.587, 0.114])
+    avg_colour = (int((sum(colour_array_red)/len(colour_array_red))*0.299) + int((sum(colour_array_green)/len(colour_array_green))*0.587) + int((sum(colour_array_blue)/len(colour_array_blue))*0.114))
 
-    print(avg_colour)
+    return avg_colour
 
-    #Updating the Colours
-    for thing3 in range(pixel_x*cell_size, pixel_x*cell_size + cell_size):
-        for thing4 in range(pixel_y*cell_size, pixel_y*cell_size + cell_size):
-            pixel[thing3, thing4] = avg_colour
+def Reducing_Pallet(gray_scale):
+    #Note Grayscale is a range from 0 to 255 (5 colour Pallet)
+     counter = 0
+     while True:
+         if gray_scale < counter:
+             gray_scale = counter - 50
+             return gray_scale
+         counter += 50
 
-    return avg_colour[0], avg_colour[1], avg_colour[2]
+def Ascii(The_Matrix):
+    The_Matrix[The_Matrix == 0  ] = '.'
+    The_Matrix[The_Matrix == 50 ] = ','
+    The_Matrix[The_Matrix == 100] = '2'
+    The_Matrix[The_Matrix == 150] = '+'
+    The_Matrix[The_Matrix == 200] = '='
+    The_Matrix[The_Matrix == 250] = '#'
 
+    return The_Matrix
 
 for thing1 in img_xrange:
     for thing2 in img_yrange:
-        The_Matrix[thing1][thing2][0], The_Matrix[thing1][thing2][1], The_Matrix[thing1][thing2][2] = Average_Colour(thing1, thing2)
+        The_Matrix[thing1][thing2] = Average_Colour(thing1, thing2)
+        The_Matrix[thing1][thing2] = Reducing_Pallet(The_Matrix[thing1][thing2])
 
-img.save(os.path.join(file_path, 'tester_update.png'))
+The_Matrix = Ascii(The_Matrix)
 
-
-#Note there is an error when the Image Dimensions arent a multiple of cell size
+print(The_Matrix)
+#img.save(os.path.join(file_path, 'tester_update.png'))
